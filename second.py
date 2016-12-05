9fb9d31d1b65e4f2d01ee478f9eb3c7c78a42a5a
GergoV <geritwo@gmail.com>
2016-12-05 10:08:04 +0100

    second.py done - may need improved error handling

--- second.py
+++ second.py
@@ -4,3 +4,17 @@
 # If any problem raises with the file output, the function should not break, but return False.
 # Example: when called with the following two parameters: "tree.txt", "apple",
 # the function should write "appleappleapple" to the file "tree.txt", and return True.
+
+def write_tentimes(file_name, text):
+    if type(file_name) == str and type(text) == str:
+        try:
+            with open(file_name, 'w') as f: # Not checking if file exists; 'w' creates/truncates in anyway
+                f.write(text*10)
+            return True
+        except Exception:
+            return False
+    else:
+        print('ERROR: Invalid input.')
+        return False
+
+write_tentimes('test.txt', 'Yolo!')
