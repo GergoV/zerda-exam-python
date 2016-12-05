ab74d2b8439354e355e9748f60d0cc86467e8b0f
GergoV <geritwo@gmail.com>
2016-12-05 09:41:14 +0100

    first.py complete.

--- first.py
+++ first.py
@@ -2,3 +2,15 @@
 # and returns a new list with every second element from the original list.
 # It should raise an error if the parameter is not a list.
 # Example: with the input [1, 2, 3, 4, 5] it should return [2, 4].
+
+def pick_from_list(list_in):
+    if type(list_in) != list:
+        raise TypeError('Please input list.')
+    else:
+        list_out = []
+        for i in range(1, len(list_in), 2):
+            list_out.append(list_in[i])
+        return list_out
+
+display_result = pick_from_list([1, 2, 3, 4, 5])
+print(display_result)
